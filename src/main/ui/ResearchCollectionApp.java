4ca5fe83093b1dc7a6626ba1ec1e0b594b36c6db
Miya Creeden <miya11@students.cs.ubc.ca>
2025-10-07 12:58:39 -0700

    Added Class-Level Documentation and Specifications for User Interface

---
+++ src/main/ui/ResearchCollectionApp.java
@@ -0,0 +1,116 @@
+package ui;
+
+// Represents a Research Collection application (user interface)
+// lets you view your reserach collection, add multiple papers,
+// compute a rating for a paper, filter the collection based off a
+// given author or displine
+
+//Credit: code written in this class is inspired by FlashcardReviewer Lab project
+// and TellerApp Project
+
+
+public class ResearchCollectionApp {
+
+    //EFFECTS: creates a new instance of the application console
+    public ResearchCollectionApp(){
+        //stub
+    }
+
+    //System.out.println("Welcome to my Research Collection!");
+
+    //MODIFIES: this
+    //EFFECTS: intializes the program with intial values
+    public void intialize(){
+        //stub
+    }
+
+    //EFFECTS: constructs the display menu for the application
+    public void diplayMainMenu(){
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: processes the user input for main menu
+    public void runMainMenu(){
+        //stub
+    }
+
+    //MODIFIES: reserach collection
+    //EFFECTS: adds a new paper to your research collection
+    public void addNewPaper(){
+        //stub
+    }
+
+
+    //MODIFIES: this
+    //EFFECTS: displays entire Reserach Collection
+    public void viewCollection(){
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: displays list of papers marked as read
+    public void viewRead(){
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: displays list of papers marked as read
+    public void viewUnread(){
+        //stub
+    }
+    
+
+
+    //EFFECTS: displays a list of commands that can be used in the 
+    // view menu
+    public void displayViewMenu(){
+        //stub
+    }
+
+    //MODIFIES: this
+    //EFFECTS: processses the user input for the view menu
+    public void runViewMenu(){
+        //stub
+    }
+
+
+
+
+    //MODIFIES: this 
+    //EFFECTS: processes a user filter input and displays a list of 
+    // papers matching the filter
+    public void viewByFilter(){
+        //stub
+    }
+
+    //MODIFIES: research paper
+    //EFFECTS: marks a paper as read
+    public void markPaperAsRead(){
+        //stub
+    } 
+
+    //MODIFIES: research paper
+    //EFFECTS: marks a paper as unread
+    public void markPaperAsUnread(){
+        //stub
+    } 
+
+    //REQUIRES: 5 ≥ (enjoy and usefull) ≥ 0
+    //MODIFIES: reserach paper
+    //EFFECTS: computes rating for paper
+    public void ratePaper(){
+        //stub
+    }
+
+    //MODIFIES: this 
+    //EFFECTS: quits application, displays closing message
+    public void quit(){
+        //stub
+    }
+
+
+
+    
+    
+}
