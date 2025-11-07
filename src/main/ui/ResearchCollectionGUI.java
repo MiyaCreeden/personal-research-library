4b1926621c7f4fd1adfa2a657d126fb7fe9ab204
Miya Creeden <miya11@students.cs.ubc.ca>
2025-11-07 12:21:37 -0800

    Added class level documentation for ResearchCollectionGUI

---
+++ src/main/ui/ResearchCollectionGUI.java
@@ -0,0 +1,52 @@
+package ui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+
+public class ResearchCollectionGUI extends JFrame{
+    
+    //EFFECTS: creates graphfical interface for reserachcollection
+    public ResearchCollectionGUI(){
+        //stub
+    }
+
+    //EFFECTS: sets button for ViewCollection
+    public JButton viewButton(){
+        return null;
+        //stub
+    }
+
+    //EFFECTS: sets button for AddPaper
+    public JButton addButton(){
+        return null;
+        //stub
+    }
+    
+    //EFFECTS: sets button for Search
+    public JButton searchButton(){
+        return null;
+        //stub
+    }
+
+    //EFFECTS: saves papers to collection
+    public JButton saveButton(){
+        return null;
+        //stub
+    }
+
+    //EFFECTS: loads data from reserach collection
+    public JButton loadButton(){
+        return null;
+        //stub
+    }
+    
+ 
+
+}
