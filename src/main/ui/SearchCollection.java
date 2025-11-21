36f4dd60dc23db0444fe0b1e2647142e83727c12
Miya Creeden <miya11@students.cs.ubc.ca>
2025-11-20 17:30:25 -0800

    Implemented SearchCollection

--- src/main/ui/SearchCollection.java
+++ src/main/ui/SearchCollection.java
@@ -1,5 +1,51 @@
 package ui;
 
-public class SearchCollection {
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.util.ArrayList;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
+import model.ResearchPaper;
+
+//Credit: code in this class was inspired by ListDialog and ListDemo
+// in Oracle (https://docs.oracle.com/javase/tutorial/uiswing/components/list.html)
+
+//creates the filtered panel with search results
+public class SearchCollection extends JPanel{
+    public JList<String> searchedList;
+    public ArrayList<ResearchPaper> results;
+    public Component parent;
+
+    //EFFECTS: instantiates a new list of only search results
+    public SearchCollection(ArrayList<ResearchPaper> filtered, Component parent){
+        this.results = filtered;
+        this.parent = parent;
+
+        setLayout(getLayout());
+        setPreferredSize(new Dimension(700, 500));
+
+       
+        String[] paperTitles = new String[results.size()];
+        int i = 0;
+        for (ResearchPaper p : results) {
+            paperTitles[i] = p.getPTitle();
+            i++;
+        }
+
+        searchedList = new JList<>(paperTitles);
+        searchedList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        searchedList.setLayoutOrientation(JList.VERTICAL_WRAP);
+
+        JScrollPane scroll = new JScrollPane(searchedList);
+        scroll.setPreferredSize(new Dimension(600, 400));
+        add(scroll, BorderLayout.CENTER);
+
+        
+
+    }
 
 }
